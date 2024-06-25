e4918bd7458763ed438db1b5d03faa23a4dfdf0f
Temur Mikava <temsonmikava@gmail.com>
2024-06-25 20:39:26 +0400

    Add shared dependencies

--- OpenAPI.Core/OpenAPI.Core/Program.cs
+++ OpenAPI.Core/OpenAPI.Core/Program.cs
@@ -1,3 +1,9 @@
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using OpenAPI.Core.Data;
+using SharedKernel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,32 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer();
+
+
+builder.Services.AddScoped<IIntegrationEventService, IntegrationEventService>();
+builder.Services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
+
+
+builder.Services.AddMassTransit(configuration =>
+{
+    configuration.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("rabbitmq", "/", h =>
+        {
+            h.Username("tmikava");
+            h.Password("Npottwyctd12");
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
